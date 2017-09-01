f519713f33c7ed9bc40ffb97f88fd3af94a844bb
jcstarter <david.bludona@gmail.com>
2017-09-01 14:14:46 -0400

    Update Notes.php

--- Notes.php
+++ Notes.php
@@ -10,3 +10,15 @@ Side Notes: Since PHP 5.3, it is possible to leave out the middle part of the te
 Source: https://davidwalsh.name/php-cookies
 */
 ?>
+
+
+<script type="text/javascript"> 
+ <!-- SessionCam Client Integration v6.0 -->
+ //<![CDATA[ 
+ var scRec=document.createElement('SCRIPT'); 
+ scRec.type='text/javascript'; 
+ scRec.src="//d2oh4tlt9mrke9.cloudfront.net/Record/js/sessioncam.recorder.js" 
+ document.getElementsByTagName('head')[0].appendChild(scRec); 
+ //]]>
+ <!-- End SessionCam -->
+</script>
